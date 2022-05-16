73c54430ef949751e5db44e8ac18e7ee1e2438c1
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2022-05-16 14:56:38 +0900

    0516

--- day0516/app.js
+++ day0516/app.js
@@ -1,3 +1,22 @@
-num1 = 1;
-num2 = 2;
-console.log(num1+num2);
+const calculator = {
+    add: function (num1, num2) {
+        return (num1+ num2);
+    },
+    minus: function (num1, num2) {
+        return Math.abs (num1-num2)
+        return (num1- num2);
+    },
+    divide: function (num1, num2) {
+        return (num1/ num2);
+    },
+    multify: function (num1, num2) {
+        return (num1* num2);
+    },
+    squared: function (num1, num2) {
+        return (num1** num2);
+    },
+    mod: function (num1, num2) {
+        return (num1% num2);
+    },
+
+}
