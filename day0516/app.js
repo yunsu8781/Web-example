d500d352d4e08160a9824497fbca2aa266156799
yunsu8781 <84833757+yunsu8781@users.noreply.github.com>
2022-05-17 15:10:43 +0900

    0517

--- day0516/app.js
+++ day0516/app.js
@@ -1,22 +1,5 @@
-const calculator = {
-    add: function (num1, num2) {
-        return (num1+ num2);
-    },
-    minus: function (num1, num2) {
-        return Math.abs (num1-num2)
-        return (num1- num2);
-    },
-    divide: function (num1, num2) {
-        return (num1/ num2);
-    },
-    multify: function (num1, num2) {
-        return (num1* num2);
-    },
-    squared: function (num1, num2) {
-        return (num1** num2);
-    },
-    mod: function (num1, num2) {
-        return (num1% num2);
-    },
+let name = "천예지"
+console.log(name)
 
-}
+name = "히히";
+console.log(name);
---
+++ day0517/example.js
@@ -0,0 +1,7 @@
+const login_link = document.querySelector("#login-link")
+
+function clickHandler () {
+    console.log("클릭")
+}
+
+login_link.addEventListener("click", clickHandler)
